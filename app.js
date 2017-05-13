a1aafb1114116b7326f1ad1212c7963d7ae1a471
Zia ul ain <m.ziaulain@gmail.com>
2017-05-13 19:27:12 +0500

    wrong check

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ angular.module('LunchCheck', [])
 
     LunchCheck.checkItems = function() {
       var itemsCount = LunchCheck.lunchItems.split(',').length;
-      if(LunchCheck.lunchItems === '')
-        LunchCheck.message = 'Enjoy!';
-      else if(itemsCount > 3)
-        LunchCheck.message = 'Too much!';
-      else {
+      if(LunchCheck.lunchItems === '' || LunchCheck.lunchItems === undefined){
         LunchCheck.message = 'Please enter data first';
+      }
+      else if(itemsCount > 3) {
+        LunchCheck.message = 'Too much!';
+      } else {
+        LunchCheck.message = 'Enjoy!';
       }
     };
 
